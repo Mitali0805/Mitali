25aa073e148d818bd215eded7fcb17b7efd280ca
Mitali0805 <60469076+Mitali0805@users.noreply.github.com>
2020-01-31 15:44:00 +0530

    Add files via upload

---
+++ Junit-Demo/src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Employee emp=new Employee();
+		emp.setId(101);
+		emp.setName("Mitali");
+		System.out.println(emp.getId()+":"+emp.getName());
+	}
+
+}
